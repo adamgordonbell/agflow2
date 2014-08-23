e4acd4fe66d4c0926923b0306d169a35cd0691c2
Adam Bell <agbell@gmail.com>
2014-08-23 16:29:50 -0400

    seems to work

--- Agflow2/src/Main.hs
+++ Agflow2/src/Main.hs
@@ -5,9 +5,7 @@ import Data.List.Split
 import Control.Monad
 import Debug.Trace
 import Data.List
-import Data.List.LCS.HuntSzymanski
 import Data.Ord
-import Data.List.Ordered
 import Data.Maybe
 import GHC.Exts
 
@@ -23,44 +21,16 @@ strLToIntL :: [String] -> Int
 strLToIntL xs = answer $ map strToInt xs
     
 answer :: [[Int]] -> Int
-answer x1@(x:xs) = foldr1 max $ (length . lis) <$> x1 
-
--- foldr1 min $ (length . lcs x) <$> xs
-
-doublelist :: [[Int]] -> [[Int]]
-doublelist [] = []
-doublelist (x:xs) = (x++x):doublelist xs
-
-t :: [[Int]] -> [[[Int]]]
-t rs = reorderings <$> rs
-
+answer (first:second:third:[]) = count
+    where
+        sp = reorderings secondRow
+        tp = reorderings thridRow
+        best = (zipWith check1) <$> (checker sp) <*> (checker tp)
+        count = head $ (length . catMaybes <$> best) ++ [1]
+        
 reorderings :: [Int] -> [[Int]]
 reorderings xs = take len . map (take len) . tails . cycle $ xs
   where len = length xs
-  
-longestSubstring ::(Eq a, Ord a) => [a] -> [a] -> [a]
-longestSubstring first second = head $ reverse $ sortBy (comparing length) comparisons
-  where comparisons = concatMap (\x -> map (sharedPrefix x) $ tails second) (tails first)
-
-  
-sharedPrefix :: (Eq a, Ord a) => [a] -> [a] -> [a]
-sharedPrefix (a:as) (b:bs)
-  | a==b = a:sharedPrefix as bs
-  | otherwise = []
-sharedPrefix _ _ = []
-
-lis :: Ord a => [a] -> [a]
-lis = maximumBy (comparing length) . map Data.List.Ordered.nub  . filter Data.List.Ordered.isSorted . subsequences                 
---    longest                    <-- unique <-- increasing    <-- all      
- 
-windowed :: Int -> [a] -> [[a]]
-windowed size ls = 
-	(case ls of 
-		[] -> []
-		x:xs -> 
-			if length ls >= size then 
-				(take size ls) : windowed size xs
-			else windowed size xs)
             
 check :: Int -> Int -> Maybe Int        
 check x y 
@@ -73,11 +43,13 @@ check1 _  Nothing = Nothing
 check1 (Just x) (Just y) = case (x ==y) of 
                             True -> Just x
                             _ -> Nothing
-                            
+          
+          
+sorter =  reverse . sortWith (length . catMaybes)            
 
 checker r = takeWhile (lenFilter) sorted
  where  checkerResults = (zipWith check firstRow) <$> r
-        sorted = reverse . sortWith (length . catMaybes) $ checkerResults
+        sorted = sorter checkerResults
         lenFilter x = length (catMaybes x) > 1
                             
 firstRow, secondRow, thridRow :: [Int]
