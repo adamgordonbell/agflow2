553c4906c64fa9e8ab2b7d2b6315f19f845908e4
Adam Bell <agbell@gmail.com>
2014-10-20 20:13:16 -0400

    cleanup

--- Agflow2/src/Main.hs
+++ Agflow2/src/Main.hs
@@ -4,10 +4,8 @@ module Main where
 
 import Control.Applicative
 import Data.List.Split
-import Control.Monad
 import Debug.Trace
 import Data.List
-import Data.Ord
 import Data.Maybe
 import GHC.Exts
 import qualified Data.Sequence
@@ -20,51 +18,50 @@ main = do
   print . strLToIntL $ input
 
 strToInt :: String -> [Int]
-strToInt s = fmap read $ splitOn " " s
+strToInt s = read <$> splitOn " " s
 
 --strLToIntL :: [String] -> Int
 strLToIntL xs = answer $ map strToInt xs
-    
+
 --answer :: [[Int]] -> Int
-answer (first:second:third:[]) = count
+answer [first, second, third] = count
     where
         sp = reorderings second
         tp = reorderings third
-        best = ((zipWith checkRowMatches) <$> (checkerConvert sp first) <*> (checkerConvert tp first))
+        best = zipWith checkRowMatches <$> checkerConvert sp first <*> checkerConvert tp first
         -- this is the answer
-        count = foldr1 max $ (length . catMaybes <$> best) ++ [1]
-        
+        count = maximum $ (length . catMaybes <$> best) ++ [1]
+answer _ = 0
+
 reorderings :: [Int] -> [[Int]]
 reorderings !xs = take len $! map (take len) . tails . cycle $ xs
   where len = length xs
-            
-check :: Int -> Int -> Maybe Int        
-check x y 
+
+check :: Int -> Int -> Maybe Int
+check x y
     | x == y = Just x
     | otherwise = Nothing
-    
-checkRowMatches :: Maybe Int -> Maybe Int -> Maybe Int        
+
+checkRowMatches :: Maybe Int -> Maybe Int -> Maybe Int
 checkRowMatches Nothing _ = Nothing
 checkRowMatches _  Nothing = Nothing
-checkRowMatches (Just x) (Just y) = case (x ==y) of 
-                            True -> Just x
-                            _ -> Nothing
-          
-          
-sorter x =  reverse1 . sortWith (length . catMaybes) $ x        
-        where reverse1 = Data.Foldable.toList . Data.Sequence.reverse . Data.Sequence.fromList 
+checkRowMatches (Just x) (Just y) = if x == y then Just x else Nothing
+
+
+sorter x =  reverse1 . sortWith (length . catMaybes) $ x
+        where reverse1 = Data.Foldable.toList . Data.Sequence.reverse . Data.Sequence.fromList
 
 checkerConvert :: [[Int]] -> [Int] -> [[Maybe Int]]
 checkerConvert r f = go
     where r1 = V.fromList `V.map` V.fromList r
           f1 =  V.fromList f
           go = unmap $ checker1 r1 f1
-          unmap x =  V.toList <$>  V.toList x 
+          unmap x =  V.toList <$>  V.toList x
 
 checker1 :: V.Vector (V.Vector Int) -> V.Vector Int -> V.Vector (V.Vector (Maybe Int))
 checker1 r f = sorted
- where  
-        checkerResults = (V.zipWith check f) `V.map` r
+ where
+        checkerResults = V.zipWith check f `V.map` r
         sorted = filterRs checkerResults
-        filterRs rs =  V.filter (lenFilter) rs
-        lenFilter x = length (catMaybes (V.toList x)) > 1       
+        filterRs =  V.filter lenFilter
+        lenFilter x = length (catMaybes (V.toList x)) > 1
