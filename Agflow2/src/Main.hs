e33adfeba93084fff97f046e19ae5519c497386e
Adam Bell <agbell@gmail.com>
2014-10-21 08:46:56 -0400

    clean up using hlint

--- Agflow2/src/Main.hs
+++ Agflow2/src/Main.hs
@@ -1,5 +1,4 @@
 {-# LANGUAGE NoMonomorphismRestriction #-}
-{-# LANGUAGE BangPatterns #-}
 module Main where
 
 import Control.Applicative
@@ -8,6 +7,7 @@ import qualified Data.Map.Strict as Map
 import Data.Map.Strict (Map)
 import qualified Data.List
 
+main :: IO ()
 main = do
   n <- readLn :: IO Int
   input <- fmap (take n . lines) getContents
@@ -17,26 +17,33 @@ strToInt :: String -> [Int]
 strToInt s = read <$> splitOn " " s
 
 strLToIntL :: Int -> [String] -> Int
-strLToIntL max xs = answer max  (fmap strToInt xs)
+strLToIntL mx xs = answer mx  (fmap strToInt xs)
 
 answer :: Int -> [[Int]] -> Int
-answer max xs = count $ countMap $ buildMap max xs
+answer mx xs = count $ countMap $ buildMap mx xs
 
 count :: Map [Int] Int -> Int
-count map = maximum $ fmap snd (Map.toList map)
+count ans = maximum $ fmap snd (Map.toList ans)
 
 countMap :: Map Int [Int] -> Map [Int] Int
-countMap ans = Map.fromListWith (+) $ fmap (\(x,y) -> (y,1)) (Map.toList ans)
+countMap ans = Map.fromListWith (+) tupleList
+ where
+    tupleList = fmap pairOne (Map.toList ans)
+    pairOne (_,y) = (y,1)
 
 buildMap ::Int -> [[Int]] -> Map Int [Int]
-buildMap max xs = Map.map (rebaseNumbers max) $ Data.List.foldl' (flip index) Map.empty xs
+buildMap mx xs = Map.map (rebaseNumbers mx) mapIndex
+  where mapIndex = Data.List.foldl' index Map.empty xs
 
-index :: [Int] -> Map Int [Int] -> Map Int [Int]
-index xs  m = Map.unionWith (++) m (Map.fromList $ pairs xs)
+index :: Map Int [Int] -> [Int] -> Map Int [Int]
+index  m xs = Map.unionWith (++) m (Map.fromList $ pairs xs)
 
 pairs :: [Int] -> [(Int,[Int])]
-pairs xs = zip xs (fmap (\x -> [x]) [1..])
+pairs xs = zip xs (fmap (: []) [1..])
 
 rebaseNumbers :: Int -> [Int] -> [Int]
-rebaseNumbers max xs@(x:_) = fmap (\y -> (y + max) `mod` max) shift
-  where shift = fmap (\y -> y - x + 1) xs
+rebaseNumbers mx xs@(x:_) = fmap (rebase . normalize) xs
+  where
+        normalize y = y - x + 1
+        rebase y = (y + mx) `mod` mx
+rebaseNumbers _ _ = error "can't rebase empty list"
