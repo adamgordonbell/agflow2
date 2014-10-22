ccddaa658d588585d57627f086272c0df24c0574
Adam Bell <agbell@gmail.com>
2014-10-22 12:46:35 -0400

    adding lots of type synonmys

--- Agflow2/src/Main.hs
+++ Agflow2/src/Main.hs
@@ -7,40 +7,43 @@ import qualified Data.Map.Strict as Map
 import Data.Map.Strict (Map)
 import qualified Data.List
 
+type Reel = [Int]
+type Positions = [Int]
+-- for each number, what are its positions in each reel
+type PositionMap = Map Int Positions
+-- numbers in reel are from 1..Max
+type Max = Int
+
 main :: IO ()
 main = do
-  n <- readLn :: IO Int
-  input <- fmap (fmap strToInt . take n . lines) getContents
-  print $ answer n input
+  mx <- readLn :: IO Max
+  reels <- fmap (fmap readReel . take 3 . lines) getContents
+  print $ answer mx reels
+  where
+    readReel s = read <$> splitOn " " s
+    answer mx xs = mostCommonPositionCount $ getPositionMap mx xs
 
-strToInt :: String -> [Int]
-strToInt s = read <$> splitOn " " s
-
-answer :: Int -> [[Int]] -> Int
-answer mx xs = count $ countMap $ buildMap mx xs
-
-count :: Map [Int] Int -> Int
-count ans = maximum $ fmap snd (Map.toList ans)
-
-countMap :: Map Int [Int] -> Map [Int] Int
-countMap ans = Map.fromListWith (+) tupleList
+mostCommonPositionCount :: PositionMap -> Int
+mostCommonPositionCount ans = maximum count
  where
+    count = fmap snd (Map.toList countMap)
+    countMap = Map.fromListWith (+) tupleList
     tupleList = fmap pairOne (Map.toList ans)
     pairOne (_,y) = (y,1)
 
-buildMap ::Int -> [[Int]] -> Map Int [Int]
-buildMap mx xs = Map.map (rebaseNumbers mx) mapIndex
+getPositionMap :: Max -> [Reel] -> PositionMap
+getPositionMap mx xs = Map.map (rebasePositions mx) mapIndex
   where mapIndex = Data.List.foldl' index Map.empty xs
 
-index :: Map Int [Int] -> [Int] -> Map Int [Int]
+index :: PositionMap -> Reel -> PositionMap
 index  m xs = Map.unionWith (++) m (Map.fromList $ pairs xs)
 
-pairs :: [Int] -> [(Int,[Int])]
+pairs :: Reel -> [(Int,[Int])]
 pairs xs = zip xs (fmap (: []) [1..])
 
-rebaseNumbers :: Int -> [Int] -> [Int]
-rebaseNumbers mx xs@(x:_) = fmap (rebase . normalize) xs
+rebasePositions :: Max -> Positions -> Positions
+rebasePositions mx xs@(x:_) = fmap (rebase . normalize) xs
   where
         normalize y = y - x + 1
         rebase y = (y + mx) `mod` mx
-rebaseNumbers _ _ = error "can't rebase empty list"
+rebasePositions _ _ = error "can't rebase empty list"
